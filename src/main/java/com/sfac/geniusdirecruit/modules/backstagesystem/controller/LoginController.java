f1448d8cf143f7153332ad215eb412676bad5027
huang_heyi <482369552@qq.com>
2021-01-05 11:29:47 +0800

    测试

--- src/main/java/com/sfac/geniusdirecruit/modules/backstagesystem/controller/LoginController.java
+++ src/main/java/com/sfac/geniusdirecruit/modules/backstagesystem/controller/LoginController.java
@@ -1,21 +1,17 @@
 package com.sfac.geniusdirecruit.modules.backstagesystem.controller;
 
-import com.sfac.geniusdirecruit.common.entity.ResultEntity;
-import com.sfac.geniusdirecruit.modules.backstagesystem.entity.User;
-import com.sfac.geniusdirecruit.modules.backstagesystem.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class LoginController {
-/*合肥姑姑一个*/
     @RequestMapping("/login")
     public String index(){
 
         return "login";
     }
-/*nujshdguihefuw*/
+
 }
