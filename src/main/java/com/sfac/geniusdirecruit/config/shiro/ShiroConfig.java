c4cc6fb3e0c84be3a40a1ce3bab779881d60db18
‘rock <894327471@qq.com>
2021-01-12 01:42:17 +0800

    shiro

--- src/main/java/com/sfac/geniusdirecruit/config/shiro/ShiroConfig.java
+++ src/main/java/com/sfac/geniusdirecruit/config/shiro/ShiroConfig.java
@@ -31,7 +31,7 @@ public class ShiroConfig {
 	@Autowired
 	private MyRealm myRealm;
 	
-/*	@Bean
+	@Bean
 	public DefaultWebSecurityManager securityManager() {
 		DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
 		securityManager.setRealm(myRealm);
@@ -39,35 +39,17 @@ public class ShiroConfig {
 		return securityManager;
 	}
 	
-	*//**
-	 * -配置shiro过滤器工厂
-	 * -----------------
-	 * -拦截权限
-	 * anon：匿名访问，无需登录
-	 * authc：登录后才能访问 ---- FormAuthenticationFilter
-	 * user：登录过能访问 ---- UserFilter
-	 * logout：登出
-	 * roles：角色过滤器
-	 * ------------------
-	 * URL匹配风格
-	 * ?：匹配一个字符，如 /admin? 将匹配 /admin1，但不匹配 /admin 或 /admin/
-	 * *：匹配零个或多个字符串，如 /admin* 将匹配 /admin 或/admin123，但不匹配 /admin/1
-	 * **：匹配路径中的零个或多个路径，如 /admin/** 将匹配 /admin/a 或 /admin/a/b
-	 * -----------------------
-	 * -方法名不能乱写，如果我们定义为别的名称，又没有添加注册过滤器的配置，
-	 * -那么shiro会加载ShiroWebFilterConfiguration过滤器，
-	 * -该过滤器会寻找shiroFilterFactoryBean，找不到会抛出异常
-	 *//*
+
 	@Bean
 	public ShiroFilterFactoryBean shiroFilterFactoryBean() {
 		ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
 		shiroFilterFactoryBean.setSecurityManager(securityManager());
-		shiroFilterFactoryBean.setLoginUrl("/manager/login");
+		shiroFilterFactoryBean.setLoginUrl("/user/login");
 		shiroFilterFactoryBean.setSuccessUrl("/common/dashboard");
 		
 		// 自定义过滤器
-		FormAuthenticationFilter managerAuthc = new FormAuthenticationFilter();
-		managerAuthc.setLoginUrl("/manager/login");
+		/*FormAuthenticationFilter managerAuthc = new FormAuthenticationFilter();
+		managerAuthc.setLoginUrl("/user/login");
 		managerAuthc.setSuccessUrl("/common/dashboard");
 		UserFilter managerUser = new UserFilter();
 		managerUser.setLoginUrl("/manager/login");
@@ -83,13 +65,13 @@ public class ShiroConfig {
 		filters.put("managerUser", managerUser);
 		filters.put("tmallAuthc", tmallAuthc);
 		filters.put("tmallUser", tmallUser);
-		shiroFilterFactoryBean.setFilters(filters);
+		shiroFilterFactoryBean.setFilters(filters);*/
 		
 		// 设置访问规则
 		Map<String, String> map = new LinkedHashMap<String, String>();
 		// 匿名访问规则
 		// ---- 静态资源文件 ----
-		map.put("/manager/css/**", "anon");
+		/*map.put("/manager/css/**", "anon");
 		map.put("/manager/images/**", "anon");
 		map.put("/manager/js/**", "anon");
 		map.put("/manager/vendors/**", "anon");
@@ -110,32 +92,27 @@ public class ShiroConfig {
 		map.put("/tmall/category/*", "anon");
 		map.put("/tmall/product/*", "anon");
 		map.put("/test/**", "anon");
-		map.put("/api/**", "anon");
+		map.put("/api/**", "anon");*/
 		
 		// 登录访问规则
-		map.put("/tmall/**", "tmallAuthc");
+		/*map.put("/tmall/**", "tmallAuthc");
 		
 		// 记住我访问规则
 		map.put("/common/dashboard", "managerUser");
 		map.put("/account/**", "managerUser");
-		map.put("/shopping/**", "managerUser");
+		map.put("/shopping/**", "managerUser");*/
 		
 		shiroFilterFactoryBean.setFilterChainDefinitionMap(map);
 		
 		return shiroFilterFactoryBean;
 	}
 	
-	*//**
-	 * --注册shiro方言，让thymeleaf支持shiro标签
-	 *//*
+
 	@Bean
 	public ShiroDialect shiroDialect(){
 		return new ShiroDialect();
 	}
-	
-	*//**
-	 * --自动代理类，支持Shiro的注解
-	 *//*
+
 	@Bean
 	@DependsOn({"lifecycleBeanPostProcessor"})
 	public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator(){
@@ -144,19 +121,14 @@ public class ShiroConfig {
 		return advisorAutoProxyCreator;
 	}
 
-    *//**
-     * --开启Shiro的注解
-     *//*
+
 	@Bean
 	public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(){
 		AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
 		authorizationAttributeSourceAdvisor.setSecurityManager(securityManager());
 		return authorizationAttributeSourceAdvisor;
 	}
-	
-	*//**
-	 * --记住我之rememberMeCookie
-	 *//*
+
 	@Bean
 	public SimpleCookie rememberMeCookie() {
 	    //这个参数是cookie的名称，对应前端的checkbox的name = rememberMe
@@ -169,9 +141,7 @@ public class ShiroConfig {
 	    return simpleCookie;
 	}
 	
-	*//**
-	 * --记住我之cookie管理器
-	 *//*
+
 	@Bean
 	public CookieRememberMeManager rememberMeManager() {
 	    CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
@@ -180,6 +150,6 @@ public class ShiroConfig {
 	    cookieRememberMeManager.setCipherService(new AesCipherService());
 	    cookieRememberMeManager.setCipherKey(cipherKey);
 	    return cookieRememberMeManager;
-	}*/
+	}
 	
 }
